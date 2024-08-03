b31e56f7cac75b990e4b09f36027f71c476fe9ef
Goel-Dhruv <dhruv.goel2804@gmail.com>
2024-08-03 16:50:57 +0530

    Update verifyToken.js

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookie.token;
+
+   if (!req.cookies) {
+    return res.status(401).json({ message: "No cookies found" });
+  }
+  const token = req.cookies.token;
 
   if (!token) return res.status(401).json({ message: "Not Authenticated Middleware!" });
 
