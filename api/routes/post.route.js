a6bbdc6fa0b73517fb6f2ca345edab2fa072a221
Goel-Dhruv <dhruv.goel2804@gmail.com>
2024-08-03 22:16:27 +0530

    Update post.route.js

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -5,9 +5,9 @@ import { addPost, deletePost, getPost, getPosts, updatePost } from "../controlle
 const router = express.Router();
 
 router.get("/", getPosts);
-router.get("/:id", getPost);
-router.post("/", addPost);
-router.put("/:id", updatePost);
-router.delete("/:id", deletePost);
+router.get("/:id", verifyToken ,getPost);
+router.post("/", verifyToken ,addPost);
+router.put("/:id",verifyToken , updatePost);
+router.delete("/:id",verifyToken , deletePost);
 
 export default router;
