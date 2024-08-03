ddb2894f84cd263864de46948bf34d5b3cdf4000
Goel-Dhruv <dhruv.goel2804@gmail.com>
2024-08-03 14:29:51 +0530

    first commit

---
+++ socket/app.js
@@ -0,0 +1,48 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExists = onlineUser.find((user) => user.userId === userId);
+  if (!userExists) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log(onlineUser);
+  });
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (receiver && receiver.socketId) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    } else {
+      console.error("Receiver not found or socketId is undefined");
+      console.log("ReceiverId:", receiverId);
+      console.log("Online Users:", onlineUser);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+io.listen(4000);
