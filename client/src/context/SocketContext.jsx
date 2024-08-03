b405c5cad4e6c0df76126e7546848531d3611aa2
Goel-Dhruv <dhruv.goel2804@gmail.com>
2024-08-03 14:46:05 +0530

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://mern-real-estate-app-aiap.onrender.com"));
   }, []);
 
   useEffect(() => {
