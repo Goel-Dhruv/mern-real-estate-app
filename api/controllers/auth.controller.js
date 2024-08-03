2c23c1fc98e738c4effaab243c9ba6f341a8b460
Goel-Dhruv <dhruv.goel2804@gmail.com>
2024-08-03 17:26:18 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -71,9 +71,12 @@ export const login = async (req,res)=>{
         httpOnly: true,
         secure:true,
         maxAge: age,
+        sameSite:none
       })
       .status(200)
       .json(userInfo);
+
+      console.log(res.cookies.token)
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to login!" });
